2de4329e1bcf82507536abed0164d0bb7bf55e94
nnien001 <nnien001@gmail.com>
2017-03-16 23:49:38 -0700

    trying to fix contact-form-handler

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -12,12 +12,14 @@ $name = $_POST['name'];
 $email_address = $_POST['email']; 
 $message = $_POST['message']; 
 
+/*
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
 $email_address))
 {
     $errors .= "\n Error: Invalid email address";
 }
+*/
 
 if( empty($errors))
 {
